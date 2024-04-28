c73cd8e8b6ceaaa1edc583788a25a23940b00274
necojy <jim2323098@gmail.com>
2024-04-29 04:33:58 +0800

    新增手機手電筒功能
    拿取手機後顯示手機畫面，按下後開啟手電筒

--- Assets/Scripts/CellPhone/CellPhoneControl.cs
+++ Assets/Scripts/CellPhone/CellPhoneControl.cs
@@ -1,11 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CellPhoneControl : MonoBehaviour
 {
     public GameObject CellPhoneCanvas;
+    public Slider slider; 
+    public GameObject spotlight; 
 
+    private void Start() 
+    {
+        spotlight.SetActive(false);
+        CellPhoneCanvas.SetActive(false);
+    }
+
+    public void OpenSpotLight()
+    {
+        spotlight.SetActive(!spotlight.active);
+        CellPhoneCanvas.SetActive(!CellPhoneCanvas.active);
+
+    }
     // 開啟Canvas
     public void ShowCanvas()
     {
