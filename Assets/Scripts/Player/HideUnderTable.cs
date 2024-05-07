1336ee019fc9d01843b9efa0a8d403e5bbf735ff
Yu-Tzu-Chien <145019149+Yu-Tzu-Chien@users.noreply.github.com>
2024-05-07 16:51:12 +0800

    躲桌子(修改旋轉、固定玩家位置)

--- Assets/Scripts/Player/HideUnderTable.cs
+++ Assets/Scripts/Player/HideUnderTable.cs
@@ -1,25 +1,30 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class HideUnderTable : MonoBehaviour
 {
-    public Vector3 HidePos;
-    public Vector3 OrigPos;
-    public Quaternion OrigRot;
+    public Vector3 hidePos;
+    public Vector3 origPos;
+    public Quaternion origRot;
+    public Quaternion flippedRotation;
     private GameObject player;
     private GameObject table;
+    private GameObject locomotion;
     private BoxCollider coll;
    
     public bool isHiding = false;
+    
 
 
     void Start()
     {
         player = GameObject.Find("XR Origin (XR Rig)");
+        locomotion = GameObject.Find("Locomotion Systeam");
         table = GameObject.Find("FreeTable");
         coll = table.GetComponent<BoxCollider>();
-        HidePos = table.transform.position;
-        HidePos.y += 1f;
+        hidePos = table.transform.position;
+        hidePos.y += 1f;
         
     }
 
@@ -27,26 +32,46 @@ public class HideUnderTable : MonoBehaviour
     {
         if (!isHiding)
         {
-            OrigPos = player.transform.position;
-            OrigRot = player.transform.rotation;
+            origPos = player.transform.position;
+            origRot = player.transform.rotation;
+            flippedRotation = Quaternion.Euler(origRot.x, origRot.eulerAngles.y + 180f, origRot.z);
+        }else{
+            player.transform.position = hidePos;
         }
     }
 
     public void Hide()
     {
+       
         isHiding = !isHiding;
         if (isHiding)
         {
-            coll.isTrigger = true;
-            player.transform.position = HidePos;
-            Quaternion flippedRotation = Quaternion.Euler(OrigRot.x, -OrigRot.y, OrigRot.z);
-            player.transform.rotation = flippedRotation;
+            StartCoroutine(MovePlayer(hidePos, Quaternion.Euler(0, 180, 0)));
         }
         else
         {
-            player.transform.position = OrigPos;
-            player.transform.rotation = OrigRot;
-            coll.isTrigger = false;
+            StartCoroutine(MovePlayer(origPos, Quaternion.identity));
         }
+    
     }
+
+    IEnumerator MovePlayer(Vector3 targetPos, Quaternion targetRot)
+    {
+        float duration = 1.0f; // 移動時間（秒）
+        float elapsedTime = 0f;
+        Vector3 startPos = player.transform.position;
+        Quaternion startRot = player.transform.rotation;
+
+        while (elapsedTime < duration)
+        {
+            player.transform.position = Vector3.Lerp(startPos, targetPos, elapsedTime / duration);
+            player.transform.rotation = Quaternion.Lerp(startRot, targetRot, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        player.transform.position = targetPos;
+        player.transform.rotation = targetRot;
+    }
+
 }
