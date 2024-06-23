d08c9f566c886856516bf06bb326afb6d093a6fe
leisurepanda <seefood0327@gmail.com>
2024-06-23 18:07:43 +0800

    Grab_bag

--- Assets/Scripts/ElevorStairOption.cs
+++ Assets/Scripts/ElevorStairOption.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ElevorStairOption : MonoBehaviour
 {
-    public Transform rotatePosition;
+    public GameObject glowbox;
+    public Transform rotatePosition1;
+    public Transform rotatePosition2;
     public Transform xr_orig;
     public Transform camera_Offset;
+    //public Quaternion targetRotation;
+    //public Vector3 targetPos;//目標位置
     public float rotateSpeed = 1f;
     public float rotateViewTime = 1f;
     public Camera mainCamera;
     public Camera optionCamera;
-    void Start()
-    {
-        StartCoroutine(MovePlayer(mainCamera.transform, optionCamera.transform, xr_orig));
-    }
+    // void Start()
+    // {
+    //     StartCoroutine(MovePlayer(mainCamera.transform, optionCamera.transform, xr_orig));
+    // }
 
     void Update()
     {
@@ -24,29 +29,71 @@ public class ElevorStairOption : MonoBehaviour
     {
         if(other.CompareTag("Player"))
         {
-            //StartCoroutine(MovePlayer(mainCamera.transform, rotatePosition));
+            optionCamera.enabled = true;
+            optionCamera.transform.position = mainCamera.transform.position;
+            optionCamera.transform.rotation = mainCamera.transform.rotation;
+            mainCamera.enabled = false;
+            
+
+            StartCoroutine(MovePlayer(optionCamera.transform, rotatePosition1, xr_orig));
+            StartCoroutine(Wait2Sec());
+            
+            //optionCamera.enabled = false;
+            //mainCamera.enabled = true;
         }
     }
-
+    IEnumerator Wait2Sec(){
+        yield return new WaitForSeconds(2f);
+    }
     IEnumerator MovePlayer(Transform initial,Transform target, Transform playerPos)
     {
-        Quaternion initialRotation = initial.rotation;
-        //Vector3 direction = target.position - initial.position;
-        //Quaternion targetRotation = Quaternion.LookRotation(direction);
-        Quaternion targetRotation = target.rotation;
-        Vector3 initialPos = playerPos.position;
+        
+        //初始角度、目標角度
+        Quaternion initialRotation = initial.rotation; 
+        Quaternion targetRotation = target.rotation; 
+        //初始位置、目標位置
+        Vector3 initialPos = initial.position;
         Vector3 targetPos = target.position;
-        targetPos.y = initialPos.y;
         float elapsedTime = 0f;
         while (elapsedTime < rotateViewTime)
         {
-            camera_Offset.rotation = Quaternion.Slerp(initialRotation, targetRotation, elapsedTime / rotateViewTime);
-            xr_orig.position = Vector3.Lerp(initialPos, targetPos, elapsedTime / rotateViewTime);
+            optionCamera.transform.rotation = Quaternion.Slerp(initialRotation, targetRotation, elapsedTime / rotateViewTime);
+            //camera_Offset.position = Vector3.Lerp(targetPos, initialPos, elapsedTime / rotateViewTime);
+            optionCamera.transform.position = Vector3.Lerp(initialPos, targetPos, elapsedTime / rotateViewTime);
             elapsedTime += Time.deltaTime * rotateSpeed;
             yield return null;
         }
-        //camera_Offset.position
-        initial.position = target.position;
+        yield return new WaitForSeconds(3f);
+        StartCoroutine(MovePlayer2(optionCamera.transform, rotatePosition2, xr_orig));
+        
+   
+        //glowbox.SetActive(false);
+
+    }
+    IEnumerator MovePlayer2(Transform initial,Transform target, Transform playerPos)
+    {
+        
+        //初始角度、目標角度
+        Quaternion initialRotation = initial.rotation; 
+        Quaternion targetRotation = target.rotation; 
+        //初始位置、目標位置
+        Vector3 initialPos = initial.position;
+        Vector3 targetPos = target.position;
+        float elapsedTime = 0f;
+        while (elapsedTime < rotateViewTime)
+        {
+            optionCamera.transform.rotation = Quaternion.Slerp(initialRotation, targetRotation, elapsedTime / rotateViewTime);
+            //camera_Offset.position = Vector3.Lerp(targetPos, initialPos, elapsedTime / rotateViewTime);
+            optionCamera.transform.position = Vector3.Lerp(initialPos, targetPos, elapsedTime / rotateViewTime);
+            elapsedTime += Time.deltaTime * rotateSpeed;
+            yield return null;
+        }
+        optionCamera.enabled = false;
+        mainCamera.enabled = true;
+       
+        
+        yield return new WaitForSeconds(3f);
+        //glowbox.SetActive(false);
 
     }
 }
